ab8732fc1404d7662988c1b08710a6f100147ff0
Anna Duma <aduma@uw.edu>
2017-05-15 00:08:00 -0400

    adds onclick form and code corrections

--- js/script.js
+++ js/script.js
@@ -57,18 +57,26 @@ function capture () {
 
 // Cart array //
 var cart = [
-  {name: "Reversible Plaid", price: 26.99, id: "item01"},
-  {name: "Fringed Plaid", price: 18.99, id: "item02"},
-  {name: "Multi Color", price: 22.99, id: "item03"},
-  {name: "Northern Lights", price: 29.99, id: "item04"},
-  {name: "Ombre Infinity", price: 11.99, id: "item05"},
-  {name: "Ashby Twill", price: 70.99, id: "item06"},
-  {name: "Wool Cable Knit", price: 49.99, id: "item07"},
-  {name: "Etro Paisley-Print Silk", price: 26.99, id: "item08"}
+  // {name: "Reversible Plaid", price: 26.99, id: "item01"},
+  // {name: "Fringed Plaid", price: 18.99, id: "item02"},
+  // {name: "Multi Color", price: 22.99, id: "item03"},
+  // {name: "Northern Lights", price: 29.99, id: "item04"},
+  // {name: "Ombre Infinity", price: 11.99, id: "item05"},
+  // {name: "Ashby Twill", price: 70.99, id: "item06"},
+  // {name: "Wool Cable Knit", price: 49.99, id: "item07"},
+  // {name: "Etro Paisley-Print Silk", price: 26.99, id: "item08"}
 ];
 
 
-function addToCart() {
-    document.ElementId("addCart").value.push;
-    console.log(length);
+function addToCart(index) {
+  cart.push(products[index]);
+  console.log(cart.length);
+}
+
+function removeFromCart(index) {
+  var cartIndex = cart.indexOf(products[index])
+  if (cartIndex >= 0) {
+    cart.splice(cartIndex, 1);
+  }
+  console.log(cart.length);
 }
