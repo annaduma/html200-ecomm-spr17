a4fad8b853589f28148dff31831f0a4366dfc366
Anna Duma <aduma@uw.edu>
2017-05-14 21:13:09 -0400

    changes onclick function

--- js/script.js
+++ js/script.js
@@ -57,18 +57,18 @@ function capture () {
 
 // Cart array //
 var cart = [
-  {name: "Reversible Plaid", price: 26.99},
-  {name: "Fringed Plaid", price: 18.99},
-  {name: "Multi Color", price: 22.99},
-  {name: "Northern Lights", price: 29.99},
-  {name: "Ombre Infinity", price: 11.99},
-  {name: "Ashby Twill", price: 70.99},
-  {name: "Wool Cable Knit", price: 49.99},
-  {name: "Etro Paisley-Print Silk", price: 26.99}
+  {name: "Reversible Plaid", price: 26.99, id: "item01"},
+  {name: "Fringed Plaid", price: 18.99, id: "item02"},
+  {name: "Multi Color", price: 22.99, id: "item03"},
+  {name: "Northern Lights", price: 29.99, id: "item04"},
+  {name: "Ombre Infinity", price: 11.99, id: "item05"},
+  {name: "Ashby Twill", price: 70.99, id: "item06"},
+  {name: "Wool Cable Knit", price: 49.99, id: "item07"},
+  {name: "Etro Paisley-Print Silk", price: 26.99, id: "item08"}
 ];
 
+
 function addToCart() {
-  var cart = document.button.addToCart.value;
-  answers.push(addToCart);
-  console.log(document.button.cart.length)
+    document.ElementId("addCart").value.push;
+    console.log(length);
 }
